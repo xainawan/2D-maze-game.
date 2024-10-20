f9adbffe0e4e6e47c77c4e0037458c4b49eecf54
xainawan <i222453@nu.edu.pk>
2024-10-20 19:29:49 +0500

    Update code.cpp
    created a dynamic grid system where the grid size can be adjusted, unlike the fixed 5x5 version. added proximity hints to help locate the key and door and included an undo feature to reverse moves. There's also a new scoring system with coin collection, and I made difficulty levels customizable.

--- code.cpp
+++ code.cpp
@@ -1,124 +1,297 @@
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 using namespace std;
 
-const int GRID_SIZE = 5; // Define the grid size (5x5 in this case)
-
+// Class Cell
 class Cell {
 public:
-    bool hasKey;
-    bool hasDoor;
-    bool hasCoin;
-    bool hasBomb;
-
-    Cell() : hasKey(false), hasDoor(false), hasCoin(false), hasBomb(false) {}
-};
-
-class Player {
-public:
-    int x, y; // Player's position
-    int movesLeft;
-    bool hasKey;
-
-    Player(int startX, int startY, int allowedMoves)
-        : x(startX), y(startY), movesLeft(allowedMoves), hasKey(false) {}
-};
-
-// Function to create a maze (5x5 for simplicity)
-void createMaze(Cell maze[GRID_SIZE][GRID_SIZE]) {
-    // Place key, door, coins, and bomb randomly
-    int keyX = rand() % GRID_SIZE;
-    int keyY = rand() % GRID_SIZE;
-    maze[keyX][keyY].hasKey = true;
-
-    int doorX = rand() % GRID_SIZE;
-    int doorY = rand() % GRID_SIZE;
-    maze[doorX][doorY].hasDoor = true;
-
-    // Add some coins
-    for (int i = 0; i < 5; i++) {
-        int coinX = rand() % GRID_SIZE;
-        int coinY = rand() % GRID_SIZE;
-        maze[coinX][coinY].hasCoin = true;
+    int value;
+    Cell* rightNeighbor;
+    Cell* belowNeighbor;
+    Cell(int inputVal) {
+        value = inputVal;
+        rightNeighbor = nullptr;
+        belowNeighbor = nullptr;
     }
+};
 
-    // Add a bomb
-    int bombX = rand() % GRID_SIZE;
-    int bombY = rand() % GRID_SIZE;
-    maze[bombX][bombY].hasBomb = true;
+// Class Coord (for position)
+struct Coord {
+public:
+    int row;
+    int col;
+    Coord(int r = 0, int c = 0) {
+        row = r;
+        col = c;
+    }
+};
+
+// Build the game board grid
+Cell* createGrid(int totalRows, int totalCols) {
+    Cell* gridStart = nullptr;
+    Cell* rowHead = nullptr;
+    Cell* prevRowHead = nullptr;
+
+    for (int r = 0; r < totalRows; ++r) {
+        Cell* leftNeighbor = nullptr;
+        for (int c = 0; c < totalCols; ++c) {
+            Cell* newCell = new Cell(0);
+            if (c == 0) rowHead = newCell;
+            if (leftNeighbor) leftNeighbor->rightNeighbor = newCell;
+            if (prevRowHead) {
+                prevRowHead->belowNeighbor = newCell;
+                prevRowHead = prevRowHead->rightNeighbor;
+            }
+            leftNeighbor = newCell;
+        }
+        prevRowHead = rowHead;
+        if (r == 0) gridStart = rowHead;
+    }
+    return gridStart;
 }
 
-// Function to display the grid
-void displayGrid(Cell maze[GRID_SIZE][GRID_SIZE], Player& player) {
-    for (int i = 0; i < GRID_SIZE; i++) {
-        for (int j = 0; j < GRID_SIZE; j++) {
-            if (i == player.x && j == player.y) {
-                cout << "P "; // Display player at current position
+// Display the grid with player and key/door positions
+void showGrid(Cell* gridStart, int totalRows, int totalCols, const Coord& player, bool hasKey) {
+    Cell* row = gridStart;
+    int rowNum = 0;
+    while (row) {
+        Cell* col = row;
+        int colNum = 0;
+        while (col) {
+            if (rowNum == player.row && colNum == player.col) {
+                cout << "P   ";
             }
             else {
-                cout << ". "; // Empty space
+                if (rowNum == 0 || rowNum == totalRows - 1 || colNum == 0 || colNum == totalCols - 1) {
+                    cout << "#   ";
+                }
+                else {
+                    cout << ".   ";
+                }
             }
+            col = col->rightNeighbor;
+            colNum++;
         }
         cout << endl;
+        row = row->belowNeighbor;
+        rowNum++;
     }
 }
 
-// Function to move the player
-void movePlayer(Player& player, char direction, Cell maze[GRID_SIZE][GRID_SIZE]) {
-    switch (direction) {
-    case 'w': // Move up
-        if (player.x > 0) player.x--;
-        break;
-    case 's': // Move down
-        if (player.x < GRID_SIZE - 1) player.x++;
-        break;
-    case 'a': // Move left
-        if (player.y > 0) player.y--;
-        break;
-    case 'd': // Move right
-        if (player.y < GRID_SIZE - 1) player.y++;
-        break;
-    default:
-        cout << "Invalid move! Use w/a/s/d to move." << endl;
-        return;
+// Generate random coordinates
+Coord randomCoord(int maxRows, int maxCols) {
+    int x = rand() % (maxRows - 2) + 1;
+    int y = rand() % (maxCols - 2) + 1;
+    return Coord(x, y);
+}
+
+// Place coins in random positions
+void distributeCoins(Coord coinLocs[], int totalCoins, int maxRows, int maxCols) {
+    for (int i = 0; i < totalCoins; ++i) {
+        coinLocs[i] = randomCoord(maxRows, maxCols);
+    }
+}
+
+// Check if a coin exists at the player's position
+bool checkCoin(const Coord& player, const Coord coinLocs[], bool pickedCoins[], int totalCoins) {
+    for (int i = 0; i < totalCoins; ++i) {
+        if (!pickedCoins[i] && player.row == coinLocs[i].row && player.col == coinLocs[i].col) {
+            pickedCoins[i] = true;
+            return true;
+        }
+    }
+    return false;
+}
+
+// Calculate distance between two coordinates
+int calcDistance(const Coord& a, const Coord& b) {
+    return abs(a.row - b.row) + abs(a.col - b.col);
+}
+
+// Give player proximity hints
+void provideHint(const Coord& player, const Coord& key, const Coord& door, int prevKeyDist, int prevDoorDist) {
+    int currKeyDist = calcDistance(player, key);
+    int currDoorDist = calcDistance(player, door);
+
+    if (currKeyDist < prevKeyDist) {
+        cout << "Hint: You're getting closer to the key.\n";
+    }
+    else if (currKeyDist > prevKeyDist) {
+        cout << "Hint: You're getting farther from the key.\n";
     }
 
-    // Check for key, door, or coin
-    if (maze[player.x][player.y].hasKey) {
-        player.hasKey = true;
-        maze[player.x][player.y].hasKey = false; // Remove the key
-        cout << "You found the key!" << endl;
+    if (currDoorDist < prevDoorDist) {
+        cout << "Hint: You're getting closer to the door.\n";
     }
-    if (maze[player.x][player.y].hasDoor && player.hasKey) {
-        cout << "You unlocked the door and escaped!" << endl;
-        player.movesLeft = 0; // End the game
+    else if (currDoorDist > prevDoorDist) {
+        cout << "Hint: You're getting farther from the door.\n";
     }
 }
 
 int main() {
-    srand(static_cast<unsigned>(time(0))); // Seed for random number generation
+    srand(time(0));
 
-    Player player(2, 2, 20); // Start player at the center (2,2)
-    Cell maze[GRID_SIZE][GRID_SIZE]; // Declare the maze
+    int totalRows, totalCols, remainingMoves, availableUndos;
+    string gameLevel;
 
-    // Create the maze
-    createMaze(maze);
+    cout << "Choose difficulty level (Easy, Medium, Hard): ";
+    cin >> gameLevel;
 
-    while (player.movesLeft > 0) {
-        cout << "Moves left: " << player.movesLeft << endl;
-        displayGrid(maze, player); // Display the grid with player's position
-
-        char input;
-        cout << "Enter move (w=up, a=left, s=down, d=right): ";
-        cin >> input;
-        movePlayer(player, input, maze); // Move the player based on input
-
-        player.movesLeft--; // Decrease the number of remaining moves
-        cout << endl; // Add some space for better readability
+    switch (tolower(gameLevel[0])) {
+    case 'e':
+        totalRows = totalCols = 10;
+        remainingMoves = 24;
+        availableUndos = 6;
+        break;
+    case 'm':
+        totalRows = totalCols = 15;
+        remainingMoves = 20;
+        availableUndos = 4;
+        break;
+    case 'h':
+        totalRows = totalCols = 20;
+        remainingMoves = 18;
+        availableUndos = 1;
+        break;
+    default:
+        cout << "Invalid input. Defaulting to Easy level.\n";
+        totalRows = totalCols = 10;
+        remainingMoves = 24;
+        availableUndos = 6;
     }
 
-    cout << "Game over! You have no more moves left." << endl;
+    Cell* grid = createGrid(totalRows, totalCols);
 
+    Coord playerPos(1, 1);
+    Coord lastPos = playerPos;
+    Coord keyLoc = randomCoord(totalRows, totalCols);
+    Coord doorLoc = randomCoord(totalRows, totalCols);
+    Coord bombLoc = randomCoord(totalRows, totalCols);
+
+    const int numCoins = 5;
+    Coord coinLocs[numCoins];
+    bool pickedCoins[numCoins] = { false };
+    distributeCoins(coinLocs, numCoins, totalRows, totalCols);
+
+    bool hasKey = false;
+    int gameScore = 0;
+    int coinsCollected = 0;
+    char command;
+
+    int prevKeyDist = calcDistance(playerPos, keyLoc);
+    int prevDoorDist = calcDistance(playerPos, doorLoc);
+
+    while (true) {
+        if (remainingMoves <= 0) {
+            cout << "Game Over: You're out of moves!\n";
+            break;
+        }
+
+        cout << "Moves left: " << remainingMoves << "\tUndos left: " << availableUndos << "\tScore: " << gameScore << "\n";
+        cout << "Coins collected: " << coinsCollected << "\n";
+        cout << "Your move (w: up, s: down, a: left, d: right, u: undo): ";
+        cin >> command;
+
+        bool playerMoved = false;
+        bool validMove = true;
+
+        switch (command) {
+        case 'w':
+            if (playerPos.row > 1) {
+                lastPos = playerPos;
+                playerPos.row--;
+                playerMoved = true;
+            }
+            else {
+                validMove = false;
+            }
+            break;
+        case 's':
+            if (playerPos.row < totalRows - 2) {
+                lastPos = playerPos;
+                playerPos.row++;
+                playerMoved = true;
+            }
+            else {
+                validMove = false;
+            }
+            break;
+        case 'a':
+            if (playerPos.col > 1) {
+                lastPos = playerPos;
+                playerPos.col--;
+                playerMoved = true;
+            }
+            else {
+                validMove = false;
+            }
+            break;
+        case 'd':
+            if (playerPos.col < totalCols - 2) {
+                lastPos = playerPos;
+                playerPos.col++;
+                playerMoved = true;
+            }
+            else {
+                validMove = false;
+            }
+            break;
+        case 'u':
+            if (availableUndos > 0) {
+                playerPos = lastPos;
+                availableUndos--;
+                cout << "Undo successful! Back to the previous position.\n";
+            }
+            else {
+                cout << "No undos left!\n";
+            }
+            continue;
+        default:
+            validMove = false;
+        }
+
+        if (!validMove) {
+            cout << "Invalid move! You hit the border.\n";
+        }
+
+        if (playerMoved) {
+            remainingMoves--;
+
+            if (checkCoin(playerPos, coinLocs, pickedCoins, numCoins)) {
+                coinsCollected++;
+                gameScore += 2;
+                remainingMoves++;
+                availableUndos++;
+                cout << "Coin collected! +2 points, +1 move, +1 undo.\n";
+            }
+
+            provideHint(playerPos, keyLoc, doorLoc, prevKeyDist, prevDoorDist);
+
+            prevKeyDist = calcDistance(playerPos, keyLoc);
+            prevDoorDist = calcDistance(playerPos, doorLoc);
+        }
+
+        showGrid(grid, totalRows, totalCols, playerPos, hasKey);
+
+        if (playerPos.row == bombLoc.row && playerPos.col == bombLoc.col) {
+            cout << "Boom! You stepped on a bomb! Game over.\n";
+            break;
+        }
+
+        if (playerPos.row == keyLoc.row && playerPos.col == keyLoc.col && !hasKey) {
+            hasKey = true;
+            cout << "You found the key! Now head to the door.\n";
+        }
+
+        if (hasKey && playerPos.row == doorLoc.row && playerPos.col == doorLoc.col) {
+            cout << "Congratulations! You unlocked the door and won the game!\n";
+            break;
+        }
+    }
+
+    delete grid;
     return 0;
 }
